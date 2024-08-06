1f56f9c4282d79b85144e6030d430a9938896976
Vo Minh Long <vominhlongbentre@gmail.com>
2024-08-06 14:24:21 +0700

    ok

--- src/commands/dictionary/lookupFunctions/dictionaryapidev.ts
+++ src/commands/dictionary/lookupFunctions/dictionaryapidev.ts
@@ -1,3 +1,8 @@
+import { 
+	Interaction, 
+	Message
+} from 'discord.js';
+
 async function getFromDictionary(word: string) {
 	try {
 		const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
@@ -41,10 +46,70 @@ export function dictionaryapidev({
 				return;
 			}
 			
-			interaction.reply(
-				`# ${value[0].word}\nPhonetic: ${value[0].phonetics[0].text}\nMeaning: ${value[0].meanings[0].definitions[0].definition}`
-			);
-		})
+			let replyMessage: String = `# ${value[0].word}\n## Phonetics\n`;
+			let splitMode: Boolean = false;
+
+			const regex = /https:\/\/api\.dictionaryapi\.dev\/media\/pronunciations\/en\/(.+)/;
+			for (const phonetic of value[0].phonetics) {
+				replyMessage += `- ${phonetic.text}`;
+				if (phonetic.audio !== "") {
+					const audioFileName = phonetic.audio.match(regex)[1];
+					const audioURL = phonetic.audio;
+					replyMessage += ` - [${audioFileName}](${audioURL})\n`;
+				} else {
+					replyMessage += "\n";
+				}
+			}
+
+			replyMessage += "## Definitions\n";
+
+			for (const eachValue of value) {
+				for (const meaning of eachValue.meanings)
+				{
+					replyMessage += `**${meaning.partOfSpeech}**\n`;
+					for (const definition of meaning.definitions) {
+						replyMessage += `- ${definition.definition}\n`;
+
+						if (definition.example !== undefined) {
+							replyMessage += `  - *Example:* ${definition.example}\n`;
+						}
+
+						if (definition.synonyms !== undefined && definition.synonyms.length > 0) {
+							replyMessage += `  - *Synonyms:* ${definition.synonyms.join(", ")}\n`;
+						}
+
+						if (definition.antonyms !== undefined && definition.antonyms.length > 0) {
+							replyMessage += `  - *Antonyms:* ${definition.antonyms.join(", ")}\n`;
+						}
+					}
+
+					if (replyMessage.length > 1900) {
+						if (splitMode === true) {
+							interaction.followUp(replyMessage);
+							replyMessage = "";
+						} else {
+							interaction.reply(replyMessage);
+							replyMessage = "";
+							splitMode = true;
+						}
+					}
+
+					if (meaning.synonyms !== undefined && meaning.synonyms.length > 0) {
+						replyMessage += `- **Synonyms:** ${meaning.synonyms.join(", ")}\n`;
+					}
+					
+					if (meaning.antonyms !== undefined && meaning.antonyms.length > 0) {
+						replyMessage += `- **Antonyms:** ${meaning.antonyms.join(", ")}\n`;
+					}
+				}
+			}
+
+			if (splitMode === true) {
+				interaction.followUp(replyMessage);
+			} else {
+				interaction.reply(replyMessage);
+			}
+		});
 	} catch (error) {
 		console.log(`ERROR:\n${error}`);
 	}
