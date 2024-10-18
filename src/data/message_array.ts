8af04ed91e164bc19249974e3fb76e774adda67c
khanglaconnguoi <tohoangkhanglaconnguoi@gmail.com>
2024-10-18 22:31:37 +0700

    Added new methods to MessageArray

--- src/data/message_array.ts
+++ src/data/message_array.ts
@@ -17,6 +17,18 @@ class messageArray {
 		}
 	};
 
+	extend(list: string[]) {
+		for (const item of list) {
+			if (item.length >= 2000) {
+				throw new Error("Cannot extend with this list because it contains an item whose length is greater than 2000 characters.");
+			}
+		}
+
+		for (const item of list) {
+			this.push(item);
+		}
+	};
+
 	length = () => {
 		return this.mainArray.length;
 	};
@@ -24,6 +36,24 @@ class messageArray {
 	get = (index: number) => {
 		return this.mainArray[index];
 	};
+
+	withPageNumber() {
+		const totalPageNumber = this.length();
+		if (totalPageNumber === 1) return this.mainArray;
+
+		let result: string[] = [];
+		for (let i = 0; i < totalPageNumber; ++i) {
+			const page = this.mainArray[i];
+			const pageWithNumber = page + `-# Trang ${i + 1}/${totalPageNumber}`;
+			result.push(pageWithNumber);
+		}
+		return result;
+	};
+
+	isEmpty() {
+		if ((this.length() === 1) && !(this.mainArray[0])) return true
+		else return false;
+	}
 };
 
 export default messageArray;
