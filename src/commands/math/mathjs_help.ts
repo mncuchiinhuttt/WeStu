71a6d1aa519e7dce16adeadd9e2e35516e05c0a4
khanglaconnguoi <tohoangkhanglaconnguoi@gmail.com>
2024-10-27 11:47:42 +0700

    Fixed a minor issue in /mathjs help

--- src/commands/math/mathjs_help.ts
+++ src/commands/math/mathjs_help.ts
@@ -7,7 +7,7 @@ export function mathjs_help({
 	const message = new messageArray();
 	message.push(`# Math.js Help\n`);
 	for (const func of commonMathFunctions) {
-		message.push(`**${func.name}** - \`${func.function}\``);
+		message.push(`**${func.name}** - \`${func.function}\`\n`);
 	}
 	message.push(`\nTo use a function, type \`mathjs evaluate\` followed by the function and the number you want to evaluate. For example, to find the square root of 4, type \`mathjs evaluate sqrt(4)\``);
 	interaction.reply(message.get(0));
