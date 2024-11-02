626afed2ceef21fa51efe1e3c6503a999c82c1ab
Vo Minh Long <vominhlongbentre@gmail.com>
2024-11-02 15:30:54 +0700

    fsdabfewbiuaf

--- src/commands/tools/summarization.ts
+++ src/commands/tools/summarization.ts
@@ -23,26 +23,30 @@ async function run ({
 	const text = interaction.options.getString('text')!;
 	await interaction.deferReply();
 	await interaction.editReply('Summarizing...');
-	query({"inputs": `${text}`}).then(async (response) => {
-		const bot_response = new messageArray();
-		console.log(response[0].summary_text);
-		bot_response.push(response[0].summary_text);
-		const message_length = bot_response.length();
-		if (message_length === 1) {
-			await interaction.editReply(bot_response.get(0));
-		} else {
-			const pages = bot_response.withPageNumber();
-			await interaction.editReply(pages[0]);
-			for (let i = 1; i < pages.length; i++) {
-				await interaction.followUp(pages[i]);
+	try {
+		query({"inputs": `${text}`}).then(async (response) => {
+			const bot_response = new messageArray();
+			bot_response.push(response[0].summary_text);
+			const message_length = bot_response.length();
+			if (message_length === 1) {
+				await interaction.editReply(bot_response.get(0));
+			} else {
+				const pages = bot_response.withPageNumber();
+				await interaction.editReply(pages[0]);
+				for (let i = 1; i < pages.length; i++) {
+					await interaction.followUp(pages[i]);
+				}
 			}
-		}
-	});
+		});
+	} catch (error) {
+		console.error(error);
+		await interaction.editReply('An error occurred while trying to summarize the text.');
+	}
 };
 
 const data = new SlashCommandBuilder()
 	.setName('summarization')
-	.setDescription('Summarize text using Facebook BART')
+	.setDescription('Summarize text using Facebook BART. This command is only supported in English.')
 	.addStringOption(option =>
 		option
 		.setName('text')
